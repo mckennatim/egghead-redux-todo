84b8b5e3c660d286113f4622c3cb9900f58c9098
mckennatim <mckenna.tim@gmail.com>
2016-01-15 11:32:02 -0500

    0-one-file

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+## tags 
+### 1-actions-reducers-components
+
+### 0-one-file
+with tests and comment headers breaking code into sections
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel?presets[]=react,presets[]=es2015'],
+      exclude: /node_modules/,
+      include: __dirname
+    }]
+  }
+}
